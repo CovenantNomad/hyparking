43dfe6d381e6a3fc63e82a046f3654dcad5989dd
Jeonghun Nam <adonis0951@hotmail.com>
2022-08-28 03:27:54 +0900

    color fix

--- src/pages/SettingPage.js
+++ src/pages/SettingPage.js
@@ -7,9 +7,7 @@ import { divisions } from '../data/division';
 
 const SettingPage = () => {
 
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
+
 
   return (
     <Container>
@@ -19,35 +17,88 @@ const SettingPage = () => {
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
           <h2 className="sr-only">Products</h2>
 
-          <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {divisions.map((division) => (
-              <li key={division.id} className="col-span-1 flex shadow-sm rounded-md">
-                <div
-                  className={classNames(
-                    division.color,
-                    'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'
-                  )}
-                >
-                  {division.short}
-                </div>
-                <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
-                  <div className="flex-1 px-4 py-2 text-sm truncate">
-                    <div className="text-gray-900 font-medium hover:text-gray-600">
-                      {division.name}
-                    </div>
-                  </div>
-                  <div className="flex-shrink-0 pr-2">
-                    <button
-                      type="button"
-                      className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      <span className="sr-only">Open options</span>
-                    </button>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </div>
+          <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            <li className="col-span-1 flex shadow-sm rounded-md">
+              <div className={'bg-[#005BD7] flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'}>
+                ALL
+              </div>
+              <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                <span className="text-gray-900 font-medium px-4 py-2 hover:text-gray-600">
+                  전체
+                </span>
+              </div>
+            </li>
+            <li className="col-span-1 flex shadow-sm rounded-md">
+              <div className={'bg-[#0080A7] flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'}>
+                MI
+              </div>
+              <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                <span className="text-gray-900 font-medium px-4 py-2 hover:text-gray-600">
+                  사역자
+                </span>
+              </div>
+            </li>
+            <li className="col-span-1 flex shadow-sm rounded-md">
+              <div className={'bg-[#154E3F] flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'}>
+                MM
+              </div>
+              <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                <span className="text-gray-900 font-medium px-4 py-2 hover:text-gray-600">
+                  남선교회
+                </span>
+              </div>
+            </li>
+            <li className="col-span-1 flex shadow-sm rounded-md">
+              <div className={'bg-[#EE4E6B] flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'}>
+                WM
+              </div>
+              <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                <span className="text-gray-900 font-medium px-4 py-2 hover:text-gray-600">
+                  여선교회
+                </span>
+              </div>
+            </li>
+            <li className="col-span-1 flex shadow-sm rounded-md">
+              <div className={'bg-[#EA5F01] flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'}>
+                YM
+              </div>
+              <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                <span className="text-gray-900 font-medium px-4 py-2 hover:text-gray-600">
+                  청장년
+                </span>
+              </div>
+            </li>
+            <li className="col-span-1 flex shadow-sm rounded-md">
+              <div className={'bg-[#F9CE0B] flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'}>
+                YC
+              </div>
+              <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                <span className="text-gray-900 font-medium px-4 py-2 hover:text-gray-600">
+                  영커플
+                </span>
+              </div>
+            </li>
+            <li className="col-span-1 flex shadow-sm rounded-md">
+              <div className={'bg-[#9972E5] flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'}>
+                IN
+              </div>
+              <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                <span className="text-gray-900 font-medium px-4 py-2 hover:text-gray-600">
+                  인터치
+                </span>
+              </div>
+            </li>
+            <li className="col-span-1 flex shadow-sm rounded-md">
+              <div className={'bg-[#C07D53] flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md'}>
+                SE
+              </div>
+              <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                <span className="text-gray-900 font-medium px-4 py-2 hover:text-gray-600">
+                  시니어
+                </span>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </Container>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -11,3 +11,7 @@ export const debounce = (callback, duration) => {
 export const getColor = (division) => {
   return divisions.filter((item) => item.name === division)[0].color
 }
+
+export function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
